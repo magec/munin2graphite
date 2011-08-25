f86a5cead222cb36edd2e9432bba86e824ebdb95
Super-User-Haiti <root@haiti.(none)>
2011-08-25 12:47:31 +0200

    Prerared to work in 1.8

--- bin/munin-graphite.rb
+++ bin/munin-graphite.rb
@@ -1,4 +1,5 @@
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__),"..","lib")))
+require 'rubygems'
 require 'munin2graphite'
 Munin2Graphite::Config.config_file = File.expand_path(File.join(File.dirname(__FILE__),"..","conf","config.yml"))
 Graphite::Base.set_connection(Munin2Graphite::Config[:carbon][:hostname])
