e22b2ca94b0d9702d996ca089355bcfa0bb6afb6
root <root@berga.uoc.es>
2011-09-05 12:19:29 +0200

    Added a ruby compilation for linux

---
+++ ruby/ruby-1.8.7-p352/lib/ruby/1.8/x86_64-linux/rbconfig.rb
@@ -0,0 +1,184 @@
+
+# This file was created by mkconfig.rb when ruby was built.  Any
+# changes made to this file will be lost the next time ruby is built.
+
+module Config
+  RUBY_VERSION == "1.8.7" or
+    raise "ruby lib version (1.8.7) doesn't match executable version (#{RUBY_VERSION})"
+
+  TOPDIR = File.dirname(__FILE__).chomp!("/lib/ruby/1.8/x86_64-linux")
+  DESTDIR = '' unless defined? DESTDIR
+  CONFIG = {}
+  CONFIG["DESTDIR"] = DESTDIR
+  CONFIG["PATCHLEVEL"] = "352"
+  CONFIG["INSTALL"] = '/usr/bin/install -c'
+  CONFIG["EXEEXT"] = ""
+  CONFIG["prefix"] = (TOPDIR || DESTDIR + "/usr/local/rvm/rubies/ruby-1.8.7-p352")
+  CONFIG["ruby_install_name"] = "ruby"
+  CONFIG["RUBY_INSTALL_NAME"] = "ruby"
+  CONFIG["RUBY_SO_NAME"] = "ruby"
+  CONFIG["MANTYPE"] = "doc"
+  CONFIG["NROFF"] = "/usr/bin/nroff"
+  CONFIG["configure_args"] = " '--prefix=/usr/local/rvm/rubies/ruby-1.8.7-p352' '--enable-shared' '--disable-install-doc'"
+  CONFIG["vendordir"] = "$(libdir)/ruby/vendor_ruby"
+  CONFIG["sitedir"] = "$(libdir)/ruby/site_ruby"
+  CONFIG["sitearch"] = "x86_64-linux"
+  CONFIG["arch"] = "x86_64-linux"
+  CONFIG["MAKEFILES"] = "Makefile"
+  CONFIG["EXPORT_PREFIX"] = ""
+  CONFIG["COMMON_HEADERS"] = ""
+  CONFIG["COMMON_MACROS"] = ""
+  CONFIG["COMMON_LIBS"] = ""
+  CONFIG["MAINLIBS"] = ""
+  CONFIG["ENABLE_SHARED"] = "yes"
+  CONFIG["DLDLIBS"] = " -lc"
+  CONFIG["SOLIBS"] = "$(LIBS)"
+  CONFIG["LIBRUBYARG_SHARED"] = "-Wl,-R -Wl,$(libdir) -L$(libdir) -l$(RUBY_SO_NAME)"
+  CONFIG["LIBRUBYARG_STATIC"] = "-l$(RUBY_SO_NAME)-static"
+  CONFIG["LIBRUBYARG"] = "$(LIBRUBYARG_SHARED)"
+  CONFIG["LIBRUBY"] = "$(LIBRUBY_SO)"
+  CONFIG["LIBRUBY_ALIASES"] = "lib$(RUBY_SO_NAME).so.$(MAJOR).$(MINOR) lib$(RUBY_SO_NAME).so"
+  CONFIG["LIBRUBY_SO"] = "lib$(RUBY_SO_NAME).so.$(MAJOR).$(MINOR).$(TEENY)"
+  CONFIG["LIBRUBY_A"] = "lib$(RUBY_SO_NAME)-static.a"
+  CONFIG["RUBYW_INSTALL_NAME"] = ""
+  CONFIG["rubyw_install_name"] = ""
+  CONFIG["LIBRUBY_DLDFLAGS"] = "-Wl,-soname,lib$(RUBY_SO_NAME).so.$(MAJOR).$(MINOR)"
+  CONFIG["LIBRUBY_LDSHARED"] = "$(CC) -shared"
+  CONFIG["debugflags"] = ""
+  CONFIG["optflags"] = ""
+  CONFIG["cflags"] = "$(optflags) $(debugflags)"
+  CONFIG["cppflags"] = ""
+  CONFIG["RDOCTARGET"] = ""
+  CONFIG["ARCHFILE"] = ""
+  CONFIG["EXTOUT"] = ".ext"
+  CONFIG["PREP"] = "miniruby$(EXEEXT)"
+  CONFIG["setup"] = "Setup"
+  CONFIG["EXTSTATIC"] = ""
+  CONFIG["STRIP"] = "strip -S -x"
+  CONFIG["TRY_LINK"] = ""
+  CONFIG["LIBPATHENV"] = "LD_LIBRARY_PATH"
+  CONFIG["RPATHFLAG"] = " -Wl,-R%1$-s"
+  CONFIG["LIBPATHFLAG"] = " -L%1$-s"
+  CONFIG["LINK_SO"] = ""
+  CONFIG["LIBEXT"] = "a"
+  CONFIG["DLEXT2"] = ""
+  CONFIG["DLEXT"] = "so"
+  CONFIG["LDSHARED"] = "$(CC) -shared"
+  CONFIG["CCDLFLAGS"] = " -fPIC"
+  CONFIG["STATIC"] = ""
+  CONFIG["ARCH_FLAG"] = ""
+  CONFIG["DLDFLAGS"] = ""
+  CONFIG["ALLOCA"] = ""
+  CONFIG["MAKEDIRS"] = "mkdir -p"
+  CONFIG["CP"] = "cp"
+  CONFIG["RM"] = "rm -f"
+  CONFIG["INSTALL_DATA"] = "$(INSTALL) -m 644"
+  CONFIG["INSTALL_SCRIPT"] = "$(INSTALL)"
+  CONFIG["INSTALL_PROGRAM"] = "$(INSTALL)"
+  CONFIG["SET_MAKE"] = ""
+  CONFIG["LN_S"] = "ln -s"
+  CONFIG["OBJDUMP"] = ""
+  CONFIG["DLLWRAP"] = ""
+  CONFIG["WINDRES"] = ""
+  CONFIG["NM"] = ""
+  CONFIG["ASFLAGS"] = ""
+  CONFIG["AS"] = "as"
+  CONFIG["AR"] = "ar"
+  CONFIG["RANLIB"] = "ranlib"
+  CONFIG["YFLAGS"] = ""
+  CONFIG["YACC"] = "bison -y"
+  CONFIG["OUTFLAG"] = "-o "
+  CONFIG["CPPOUTFILE"] = "-o conftest.i"
+  CONFIG["GNU_LD"] = "yes"
+  CONFIG["EGREP"] = "/bin/grep -E"
+  CONFIG["GREP"] = "/bin/grep"
+  CONFIG["CPP"] = "gcc -E"
+  CONFIG["OBJEXT"] = "o"
+  CONFIG["CPPFLAGS"] = " $(DEFS) $(cppflags)"
+  CONFIG["LDFLAGS"] = "-L.  -rdynamic -Wl,-export-dynamic"
+  CONFIG["CFLAGS"] = "-g -O2  -fPIC $(cflags)"
+  CONFIG["CC"] = "gcc"
+  CONFIG["target_os"] = "linux"
+  CONFIG["target_vendor"] = "unknown"
+  CONFIG["target_cpu"] = "x86_64"
+  CONFIG["target"] = "x86_64-unknown-linux-gnu"
+  CONFIG["host_os"] = "linux-gnu"
+  CONFIG["host_vendor"] = "unknown"
+  CONFIG["host_cpu"] = "x86_64"
+  CONFIG["host"] = "x86_64-unknown-linux-gnu"
+  CONFIG["build_os"] = "linux-gnu"
+  CONFIG["build_vendor"] = "unknown"
+  CONFIG["build_cpu"] = "x86_64"
+  CONFIG["build"] = "x86_64-unknown-linux-gnu"
+  CONFIG["TEENY"] = "7"
+  CONFIG["MINOR"] = "8"
+  CONFIG["MAJOR"] = "1"
+  CONFIG["target_alias"] = ""
+  CONFIG["host_alias"] = ""
+  CONFIG["build_alias"] = ""
+  CONFIG["LIBS"] = "-lrt -ldl -lcrypt -lm "
+  CONFIG["ECHO_T"] = ""
+  CONFIG["ECHO_N"] = "-n"
+  CONFIG["ECHO_C"] = ""
+  CONFIG["DEFS"] = ""
+  CONFIG["mandir"] = "$(datarootdir)/man"
+  CONFIG["localedir"] = "$(datarootdir)/locale"
+  CONFIG["libdir"] = "$(exec_prefix)/lib"
+  CONFIG["psdir"] = "$(docdir)"
+  CONFIG["pdfdir"] = "$(docdir)"
+  CONFIG["dvidir"] = "$(docdir)"
+  CONFIG["htmldir"] = "$(docdir)"
+  CONFIG["infodir"] = "$(datarootdir)/info"
+  CONFIG["docdir"] = "$(datarootdir)/doc/$(PACKAGE)"
+  CONFIG["oldincludedir"] = "/usr/include"
+  CONFIG["includedir"] = "$(prefix)/include"
+  CONFIG["localstatedir"] = "$(prefix)/var"
+  CONFIG["sharedstatedir"] = "$(prefix)/com"
+  CONFIG["sysconfdir"] = "$(prefix)/etc"
+  CONFIG["datadir"] = "$(datarootdir)"
+  CONFIG["datarootdir"] = "$(prefix)/share"
+  CONFIG["libexecdir"] = "$(exec_prefix)/libexec"
+  CONFIG["sbindir"] = "$(exec_prefix)/sbin"
+  CONFIG["bindir"] = "$(exec_prefix)/bin"
+  CONFIG["exec_prefix"] = "$(prefix)"
+  CONFIG["PACKAGE_URL"] = ""
+  CONFIG["PACKAGE_BUGREPORT"] = ""
+  CONFIG["PACKAGE_STRING"] = ""
+  CONFIG["PACKAGE_VERSION"] = ""
+  CONFIG["PACKAGE_TARNAME"] = ""
+  CONFIG["PACKAGE_NAME"] = ""
+  CONFIG["PATH_SEPARATOR"] = ":"
+  CONFIG["SHELL"] = "/bin/sh"
+  CONFIG["ruby_version"] = "$(MAJOR).$(MINOR)"
+  CONFIG["rubylibdir"] = "$(libdir)/ruby/$(ruby_version)"
+  CONFIG["archdir"] = "$(rubylibdir)/$(arch)"
+  CONFIG["sitelibdir"] = "$(sitedir)/$(ruby_version)"
+  CONFIG["sitearchdir"] = "$(sitelibdir)/$(sitearch)"
+  CONFIG["vendorlibdir"] = "$(vendordir)/$(ruby_version)"
+  CONFIG["vendorarchdir"] = "$(vendorlibdir)/$(sitearch)"
+  CONFIG["topdir"] = File.dirname(__FILE__)
+  MAKEFILE_CONFIG = {}
+  CONFIG.each{|k,v| MAKEFILE_CONFIG[k] = v.dup}
+  def Config::expand(val, config = CONFIG)
+    val.gsub!(/\$\$|\$\(([^()]+)\)|\$\{([^{}]+)\}/) do |var|
+      if !(v = $1 || $2)
+	'$'
+      elsif key = config[v = v[/\A[^:]+(?=(?::(.*?)=(.*))?\z)/]]
+	pat, sub = $1, $2
+	config[v] = false
+	Config::expand(key, config)
+	config[v] = key
+	key = key.gsub(/#{Regexp.quote(pat)}(?=\s|\z)/n) {sub} if pat
+	key
+      else
+	var
+      end
+    end
+    val
+  end
+  CONFIG.each_value do |val|
+    Config::expand(val)
+  end
+end
+RbConfig = Config # compatibility for ruby-1.9
+CROSS_COMPILING = nil unless defined? CROSS_COMPILING
