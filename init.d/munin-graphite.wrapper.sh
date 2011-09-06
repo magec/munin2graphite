6e7953a2e1c9f0b5474016a8e6b03c71a1647e78
root <root@berga.uoc.es>
2011-09-06 11:53:26 +0200

    Changed the location of the ruby, now there is no local dependency at all

--- init.d/munin-graphite.wrapper.sh
+++ init.d/munin-graphite.wrapper.sh
@@ -18,9 +18,9 @@ if [ ! -d /usr/local/rvm/rubies ]; then
     mkdir -p /usr/local/rvm/rubies
     ln -s /server/graphite/ruby/ruby-1.8.7-p352 /usr/local/rvm/rubies/ruby-1.8.7-p352
 fi
-export PATH=/server/graphite/ruby/ruby-1.8.7-p352/bin:$PATH:/usr/local/bin
-export LD_LIBRARY_PATH=/server/graphite/ruby/ruby-1.8.7-p352/lib/
-export GEM_PATH=/usr/local/rvm/gems/ruby-1.8.7-p352
+export PATH=/server/graphite/ruby/rubies/ruby-1.8.7-p352/bin:$PATH:/usr/local/bin
+export LD_LIBRARY_PATH=/server/graphite/ruby/rubies/ruby-1.8.7-p352/lib/
+export GEM_PATH=/server/graphite/ruby/gems/ruby-1.8.7-p352
 export MY_RUBY_HOME=/server/graphite/ruby/gems/ruby-1.8.7-p352/
 /server/graphite/init.d/munin2graphite $@
 
