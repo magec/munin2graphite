e73a6ba9cf2af1e1d7fe78cba103c698630903ee
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2011-09-07 11:06:07 +0200

    Added solaris support

--- init.d/munin-graphite.wrapper.sh
+++ init.d/munin-graphite.wrapper.sh
@@ -9,18 +9,8 @@
 # Short-Description: Populates carbon agents with munin-node data
 # Description:    munin graphs to graphite servers
 ### END INIT INFO
-if [ ! -d /usr/local/rvm/gems ]; then
-    mkdir -p /usr/local/rvm/gems/
-    ln -s /server/graphite/ruby/gems/ruby-1.8.7-p352/ /usr/local/rvm/gems/ruby-1.8.7-p352
-fi
-
-if [ ! -d /usr/local/rvm/rubies ]; then
-    mkdir -p /usr/local/rvm/rubies
-    ln -s /server/graphite/ruby/ruby-1.8.7-p352 /usr/local/rvm/rubies/ruby-1.8.7-p352
-fi
 export PATH=/server/graphite/ruby/rubies/ruby-1.8.7-p352/bin:$PATH:/usr/local/bin
 export LD_LIBRARY_PATH=/server/graphite/ruby/rubies/ruby-1.8.7-p352/lib/
 export GEM_PATH=/server/graphite/ruby/gems/ruby-1.8.7-p352
 export MY_RUBY_HOME=/server/graphite/ruby/gems/ruby-1.8.7-p352/
 /server/graphite/init.d/munin2graphite $@
-
