b38ab3f03fecd18e159765a1243e5a7722f112e2
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2011-09-07 11:01:28 +0200

    Now a better approach is used so the config file is passed as argument

--- bin/munin-graphite.rb
+++ bin/munin-graphite.rb
@@ -1,7 +1,12 @@
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__),"..","lib")))
 require 'rubygems'
 require 'munin2graphite'
-Munin2Graphite::Config.config_file = File.expand_path(File.join(File.dirname(__FILE__),"..","conf","config.yml"))
+
+if ARGV[0] && File.stat(ARGV[0])
+  Munin2Graphite::Config.config_file = ARGV[0]
+else
+  Munin2Graphite::Config.config_file = File.expand_path(File.join(File.dirname(__FILE__),"..","conf","config.yml"))
+end
 Graphite::Base.set_connection(Munin2Graphite::Config[:carbon][:hostname])
 Graphite::Base.authenticate(Munin2Graphite::Config[:graphite][:user],Munin2Graphite::Config[:graphite][:password])
 
