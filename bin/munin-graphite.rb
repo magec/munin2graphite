152d7bafb99b33819b05a887cbb113d9c1c93a6c
Super-User-Haiti <root@haiti.(none)>
2011-09-08 15:44:52 +0200

    Solaris stuff

--- bin/munin-graphite.rb
+++ bin/munin-graphite.rb
@@ -1,9 +1,10 @@
+#!/usr/bin/env ruby
 $:.unshift(File.expand_path(File.join(File.dirname(__FILE__),"..","lib")))
 require 'rubygems'
 require 'munin2graphite'
 
-if ARGV[0] && File.stat(ARGV[0])
-  Munin2Graphite::Config.config_file = ARGV[0]
+if ARGV.last && File.stat(ARGV.last)
+  Munin2Graphite::Config.config_file = ARGV.last
 else
   Munin2Graphite::Config.config_file = File.expand_path(File.join(File.dirname(__FILE__),"..","conf","config.yml"))
 end
