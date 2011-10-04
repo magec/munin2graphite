0afea18327c91772e6aa31848ea835c4d5e31c7b
root <root@doctor.uoc.es>
2011-10-04 14:31:04 +0200

    Now the configuration of the graphite backend is made in every worker

--- bin/munin-graphite.rb
+++ bin/munin-graphite.rb
@@ -8,8 +8,6 @@ if ARGV.last && File.stat(ARGV.last)
 else
   Munin2Graphite::Config.config_file = File.expand_path(File.join(File.dirname(__FILE__),"..","conf","config.yml"))
 end
-Graphite::Base.set_connection(Munin2Graphite::Config[:carbon][:hostname])
-Graphite::Base.authenticate(Munin2Graphite::Config[:graphite][:user],Munin2Graphite::Config[:graphite][:password])
 
 scheduler = Munin2Graphite::Scheduler.new(Munin2Graphite::Config)
 scheduler.start
