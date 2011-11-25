364bc5e984e6ce1fc3b2d44294bd60dc96f2fb1f
root <root@doctor.uoc.es>
2011-11-25 11:06:28 +0100

    Added some dependencies

--- lib/munin2graphite.rb
+++ lib/munin2graphite.rb
@@ -1,4 +1,8 @@
 $:.unshift(File.join(File.expand_path(File.dirname(__FILE__),"munin2graphite")))
+require 'rubygems'
+require 'munin-ruby'
 require 'carbon'
+require 'graphite'
+require 'munin_graph'
 require 'munin2graphite/config'
 require 'munin2graphite/scheduler'
