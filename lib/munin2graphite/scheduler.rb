03dd1532eedf03daf5b47e0521f961a498ca7c31
Jose Fernandez (magec) <jfernandezperez@gmail.com>
2012-01-13 13:32:54 +0100

    Improved the way the scheduling is made

--- lib/munin2graphite/scheduler.rb
+++ lib/munin2graphite/scheduler.rb
@@ -26,42 +26,47 @@ module Munin2Graphite
     def initialize(config)
       @config = config
     end
-    
+
     def category_from_config(config)
       config.each_line do |configline|
         if configline =~ /^graph_category ([\w\-_]+)$/
           return configline.split[1]
         end
-      end      
+      end
       raise "CategoryNotFound in #{config}"
     end
 
     def munin_config
-      return @munin_config if @munin_config 
+      return @munin_config if @munin_config
       @munin_config = {}
-      
+      @munin_config[:workers] = []
       workers.each do |worker|
-        @munin_config[worker] = {}
-        config = @config.config_for_worker(worker)
-        munin  = Munin::Node.new(config["munin_hostname"],config["munin_port"])
-        nodes = config["munin_nodes"] ? config["munin_nodes"].split(",") : munin.nodes
-        @munin_config[worker][:nodes] = {}
-        nodes.each do |node|
-
-          @munin_config[worker][:nodes][node] = {:metrics => munin.list(node)}
-          @munin_config[worker][:nodes][node][:categories] = {}
-
-          @munin_config[worker][:nodes][node][:metrics].each do |metric|
-            @munin_config[worker][:nodes][node][:config] = munin.config(metric)[metric]
-            @munin_config[worker][:nodes][node][:raw_config] = munin.config(metric,true)[metric]
-            @munin_config[worker][:nodes][node][:categories][metric] = category_from_config(@munin_config[worker][:nodes][node][:raw_config])
+        begin
+          current_config = {}
+          config = @config.config_for_worker(worker)
+          munin  = Munin::Node.new(config["munin_hostname"],config["munin_port"])
+          nodes = config["munin_nodes"] ? config["munin_nodes"].split(",") : munin.nodes
+          current_config[:nodes] = {}
+          nodes.each do |node|
+            current_config[:nodes][node] = {:metrics => munin.list(node)}
+            current_config[:nodes][node][:categories] = {}
+            current_config[:nodes][node][:metrics].each do |metric|
+              current_config[:nodes][node][:config] = munin.config(metric)[metric]
+              current_config[:nodes][node][:raw_config] = munin.config(metric,true)[metric]
+              current_config[:nodes][node][:categories][metric] = category_from_config(current_config[:nodes][node][:raw_config])
+            end
           end
-
+          @munin_config[worker] = current_config
+          @munin_config[:workers] << worker
+        rescue Exception
+          config.log.error("Error when trying to obtain graph conf. Ignored")
+          config.log.error $!
         end
         munin.disconnect
       end
+      @config.log.debug(@munin_config.inspect)
       @munin_config
-    end      
+    end
 
     def workers
       @workers ||= (@config.workers.empty? ?  ["global"] : @config.workers )
@@ -71,20 +76,15 @@ module Munin2Graphite
     # This is the loop of the metrics scheduling
     def obtain_metrics(worker = "global")
       config = @config.config_for_worker("global")
-      #      config.log.info("Obtaining metrics configuration")
-      munin_config
-      #     config.log.info("Getting metrics")
       time = Time.now
       config = @config.config_for_worker(worker)
       config.log.info("Worker #{worker}")
-      
       metric_base = config["graphite_metric_prefix"]
-      
       munin_config[worker][:nodes].keys.each do |node|
         node_name = metric_base + "." + node.split(".").first
         config.log.debug("Doing #{node_name}")
-        values = {}                       
-        config.log.debug("Asking for: #{node}")		
+        values = {}
+        config.log.debug("Asking for: #{node}")
         metric_time = Time.now
         metrics = munin_config[worker][:nodes][node][:metrics]
         config.log.debug("Metrics " + metrics.join(","))
@@ -95,14 +95,14 @@ module Munin2Graphite
             local_munin  = Munin::Node.new(config["munin_hostname"],config["munin_port"])
             values[metric] =  local_munin.fetch metric
             local_munin.disconnect
-          end            
-        end 
+          end
+        end
         metrics_threads.each {|i| i.join;i.kill}
-        config.log.info("Done with: #{node} (#{Time.now - metric_time} s)")	
+        config.log.info("Done with: #{node} (#{Time.now - metric_time} s)")
         carbon = Carbon.new(config["carbon_hostname"],config["carbon_port"])
         string_to_send = ""
-        values.each do |metric,results|          
-          category = munin_config[worker][:nodes][node][:categories][metric] 
+        values.each do |metric,results|
+          category = munin_config[worker][:nodes][node][:categories][metric]
           results.each do |k,v|
             v.each do |c_metric,c_value|
               string_to_send += "#{node_name}.#{category}.#{metric}.#{c_metric} #{c_value} #{Time.now.to_i}\n".gsub("-","_")  if c_value != "U"
@@ -118,46 +118,39 @@ module Munin2Graphite
     end
 
     def obtain_graphs
-      workers.each do |worker|
+      munin_config
+      munin_config[:workers].each do |worker|
         time = Time.now
         config = @config.config_for_worker worker
-        config.log.info("Begin : Sending Graph Information to Graphite for worker #{worker}")
-        begin
-          munin = Munin::Node.new(config["munin_hostname"],config["munin_port"])
-          Graphite::Base.set_connection(config["graphite_endpoint"])
-          Graphite::Base.authenticate(config["graphite_user"],config["graphite_password"])
-          
-          nodes = config["munin_nodes"] ? config["munin_nodes"].split(",") : munin.nodes
-          nodes.each do |node|
-            config.log.info("Graphs for #{node}")
-            munin.list(node).each do |metric|
-              config.log.info("Configuring #{metric}")
-              munin_graph = MuninGraph.graph_for munin.config(metric,true)[metric]
-              munin_graph.config = config.merge("metric" => "#{metric}","hostname" => node.split(".").first)
-              config.log.debug("Saving graph #{metric}")
-              munin_graph.to_graphite.save!
-            end
+        @config.log.info("Begin : Sending Graph Information to Graphite for worker #{worker}")
+        Graphite::Base.set_connection(config["graphite_endpoint"])
+        Graphite::Base.authenticate(config["graphite_user"],config["graphite_password"])
+        munin_config[worker][:nodes].keys.each do |node|
+          @config.log.info("Graphs for #{node}")
+          metrics = munin_config[worker][:nodes][node][:metrics]
+          metrics.each do |metric|
+            @config.log.info("Configuring #{metric}")
+            munin_graph = MuninGraph.graph_for munin_config[worker][:nodes][node][:raw_config]
+            munin_graph.config = config.merge("metric" => "#{metric}","hostname" => node.split(".").first)
+            @config.log.debug("Saving graph #{metric}")
+            munin_graph.to_graphite.save!
           end
-          config.log.info("End : Sending Graph Information to Graphite for worker #{worker}, elapsed time (#{Time.now - time}s)")
-          munin.disconnect
-        rescue Exception
-          config.log.error("Error when trying to obtain graph conf. Ignored")
-          config.log.error $!
         end
-      end    
+        config.log.info("End : Sending Graph Information to Graphite for worker #{worker}, elapsed time (#{Time.now - time}s)")
+      end
     end
-    
+
     def start
       @config.log.info("Scheduler started")
       obtain_graphs
       @scheduler = Rufus::Scheduler.start_new
-      workers.each do |worker|        
-        config = @config.config_for_worker worker        
+      workers.each do |worker|
+        config = @config.config_for_worker worker
         @config.log.info("Scheduling worker #{worker} every  #{config["scheduler_metrics_period"]} ")
         @scheduler.every config["scheduler_metrics_period"] do
           obtain_metrics(worker)
         end
       end
     end
-  end  
+  end
 end
